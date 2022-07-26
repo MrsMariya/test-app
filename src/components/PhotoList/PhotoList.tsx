a22e673b0776369b2c98894c65b76e37f1ee6969
Mariya Pastushonok <mariyayaromchik@mail.ru>
2022-07-26 17:13:26 +0300

    feat: add deleting an image

--- src/components/PhotoList/PhotoList.tsx
+++ src/components/PhotoList/PhotoList.tsx
@@ -1,13 +1,21 @@
 import { PhotoType } from '../../utils/types';
 
-export const PhotoList = ({ photos }: PhotoType) => {
+type MyProps = PhotoType & { handleDeletePhoto: (name: string) => void };
+
+export const PhotoList = ({ photos, handleDeletePhoto }: MyProps) => {
   return (
-    <div className="card-block__photos-items">
+    <div className="photos-items">
       {photos.map((photo) => {
         const { thumbpath, name } = photo;
         return (
-          <div key={name} className="card-block__photo-wrapper">
+          <div key={name} className="photo-wrapper">
             <img src={thumbpath} alt={name} />
+            <button
+              type="button"
+              aria-label="Delete"
+              className="delete_btn"
+              onClick={() => handleDeletePhoto(name)}
+            />
           </div>
         );
       })}
--- src/components/photos/Photos.tsx
+++ src/components/photos/Photos.tsx
@@ -4,7 +4,6 @@ import addFile from '../../assets/svg/Add.svg';
 import { PhotoList } from '../PhotoList/PhotoList';
 import { URLImage } from '../../utils/constants';
 import { PhotosElType, PhotoType } from '../../utils/types';
-import EditButton from '../EditButton/EditButton';
 
 const Photos = ({ photos, id }: PhotoType) => {
   const token = sessionStorage.getItem('token');
@@ -13,6 +12,13 @@ const Photos = ({ photos, id }: PhotoType) => {
   const [error, setError] = useState('');
   const [photo, setPhoto] = useState<Array<PhotosElType>>(photos);
   const [isLoading, setIsLoading] = useState(true);
+  const [currentPhoto, setCurrentPhoto] = useState('');
+  const config = {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+      Authorization: `${token}`,
+    },
+  };
 
   const handleFileSelect = (event: SyntheticEvent) => {
     event.preventDefault();
@@ -20,12 +26,6 @@ const Photos = ({ photos, id }: PhotoType) => {
     if (file) {
       formDate.append('file', file);
       formDate.append('name', file?.name);
-      const config = {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-          Authorization: `${token}`,
-        },
-      };
       axios
         .post(URLImage, formDate, config)
         .then((res) => {
@@ -41,14 +41,32 @@ const Photos = ({ photos, id }: PhotoType) => {
     }
   };
 
+  const handleDeletePhoto = (name: string) => {
+    const findPhoto = photo.find((el) => el.name === name);
+    if (findPhoto) {
+      setCurrentPhoto(findPhoto.name);
+      axios
+        .delete(`${URLImage}/${findPhoto.name}`, config)
+        .then(() => {
+          const index = photos.indexOf(findPhoto);
+          // photos.filter((el) => el.name !== findPhoto.name);
+          photos.splice(index, 1);
+          setPhoto(photos);
+          setIsLoading(true);
+        })
+        .catch((err) => {
+          setError(err.message);
+          setIsLoading(false);
+        });
+      setIsLoading(false);
+    }
+  };
+
   return (
     <div className="card-block__photos">
-      <div className="card-block__info-title">
-        Приложенные фото
-        <EditButton />
-      </div>
+      <div className="card-block__info-title">Приложенные фото</div>
       {!isLoading && <div className="loading" />}
-      <PhotoList photos={photo} key={id} id={id} />
+      <PhotoList photos={photo} key={id} id={id} handleDeletePhoto={handleDeletePhoto} />
       {error && <p style={{ color: 'red' }}>{error}</p>}
       <label htmlFor="file">
         <img src={addFile} alt="add file" />
